2951d5fc128dd570e79b4f0f56878b4175daa5a3
Kashan Shaikh <shaikhkashan670@gmail.com>
2024-02-10 14:22:01 +0500

    by kashan

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+// kashan connected
 Route::get('/', function () {
     return view('welcome');
 });
